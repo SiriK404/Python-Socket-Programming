52f23402952b64c4438ac5b7cfbdf5a9316c2318
SiriK404 <sirishakotikalapudi94@gmail.com>
2019-12-19 09:03:05 +0530

    Add files via upload

--- Simple Server-Client/server.py
+++ Simple Server-Client/server.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
 
+import socket
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((socket.gethostname(), 4444))
+s.listen(5)
+
+while True:
+    clientsocket, addr = s.accept()
+    
+    while True:
+        data = clientsocket.recv(4096)
+        if not data: break
+        
+        print(data)
+        clientsocket.send(bytes("hello "+ str(data),'utf-8'))
+    clientsocket.close()
+    print ('client disconnected')
