4b450e111c7d96680ca8124aafa07a75aead02b7
SiriK404 <sirishakotikalapudi94@gmail.com>
2019-12-19 08:58:12 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import socket
+
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.connect((socket.gethostname(),4444))
+s.send(bytes("I am CLIENt",'utf-8'))
+
+msg=s.recv(1024)
+
+s.close()
+print(msg.decode('utf-8'))
