0c70ce0e8eae29a69ad8e4d6db972fdc4e56a4a2
SiriK404 <sirishakotikalapudi94@gmail.com>
2019-12-26 12:38:30 +0530

    Update client.py

--- server-exercise3/client.py
+++ server-exercise3/client.py
@@ -1,1 +1,17 @@
+import socket
 
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.connect(('127.0.0.1',1000))
+s.send(bytes("I am client",'utf-8'))
+
+msg=s.recv(4096)
+
+print(msg.decode('utf-8'))
+s.send(bytes("how is the weather?",'utf-8'))
+msg=s.recv(4096)
+
+
+print(msg.decode('utf-8'))
+s.close()
